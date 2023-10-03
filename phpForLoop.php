4fac813887d23c0357880924d539df8bfa80ff8b
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2023-10-03 18:05:23 +0100

    added a program to count to 10

---
+++ phpForLoop.php
@@ -0,0 +1,11 @@
+<?php
+#phpForLoop - put the name of the file/program here - notes the hashtag indicates a comment in PHP
+#put your name and the date here
+#put a descripition of what the code does here - 
+#this program uses a loop to count up to 10 and puts the value of the loop counter out to the screen on each iteration
+for ($i=1;$i<=10;$i++) {
+    echo "$i, ";
+}
+
+?>
+
